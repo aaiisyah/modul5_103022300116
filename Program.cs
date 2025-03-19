1ea4f05e462f543e477bf42c6a353b99a7198bf1
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 07:54:57 +0700

    menambahkan class SimpleDataBase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+
+    }
+
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+
+    public void printAllData()
+    {
+        for(int i = 0; i <storedData.Count; i++)
+        {
+            Console.WriteLine("Data " + (i+1) + " berisi: " + storedData[i] + ", yang disimpan pada waktu UTC: " + inputDates[i]);
+        }
+        
+    }
+    
+    
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        SimpleDataBase<int> db = new SimpleDataBase<int>();
+        db.AddNewData(30);
+        db.AddNewData(1);
+        db.AddNewData(6);
+        db.printAllData();
+
+
+    }
+}
--- obj/Debug/net8.0/modul5_103022300116.AssemblyInfo.cs
+++ obj/Debug/net8.0/modul5_103022300116.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul5_103022300116")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+753007a5099057f0dafc11bcc4ff163553bdf947")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul5_103022300116")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul5_103022300116")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
